01869566c95f04d5c9f94d067f609d9159633a11
AlanBretelle <bretellealan@hotmail.fr>
2020-07-08 11:07:20 +0200

    commit init
    commit

---
+++ Assets/Pool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectType
+{
+    Player,
+    Ennemy
+}
+
+public class Pool
+{
+    private List<PoolableObject> poolableObjects;
+
+    public void Initialize(PoolableObject a, int parNumber)
+    {
+        poolableObjects = new List<PoolableObject>(parNumber);
+        poolableObjects.Add(a);
+    }
+
+    public PoolableObject getObject(int index)
+    {
+        return poolableObjects[index];
+    }
+}
---
+++ Assets/PoolableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolableObject : MonoBehaviour
+{
+    private bool isInPool;
+    
+    // Start is called before the first frame update
+     void Init()
+    {
+        gameObject.SetActive(true);
+    }
+
+     void DeInit()
+     {
+         gameObject.SetActive(false);
+     }
+}
