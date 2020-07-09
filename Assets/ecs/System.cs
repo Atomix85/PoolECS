836fb2ca4d5a69dfaeac6caf0bcf0d024c60713b
AlanBretelle <bretellealan@hotmail.fr>
2020-07-09 14:02:57 +0200

    fix system

--- Assets/ecs/System.cs
+++ Assets/ecs/System.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class System : IUpdater
+public class SystemUpdate : IUpdater
 {
     public void Run()
     {
         Accessor<Composant> modules = new Accessor<Composant>();
         foreach (var module in modules.GetAllModules())
         {
-            GameObject go = module;
+            //GameObject go = module;
             
         }
     }
