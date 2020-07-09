86b2c8240d1bcd133a7c12ee0ab7d7cf3b69a93d
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2020-07-09 16:27:28 +0200

    Tried to add the AI for Pac-Man

--- Assets/ecs/System.cs
+++ Assets/ecs/System.cs
@@ -7,9 +7,17 @@ public class SystemUpdate : IUpdater
     public void Run()
     {
         Accessor<FollowTarget> modules = Accessor<FollowTarget>.Instance();
+        
         foreach (var module in modules.GetAllModules())
         {
             module.Run();
+        }
+        
+        Accessor<TargetEdible> modulesBis = Accessor<TargetEdible>.Instance();
+        TargetEdible _nearest=null;
+        foreach (var moduleBis in modulesBis.GetAllModules())
+        {
+            moduleBis.Run();
         }
     }
 }
---
+++ Assets/ecs/TargetEdible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TargetEdible : Composant
+{
+
+    public GameObject player;
+    public NavMeshAgent _Navmesh;
+
+    public void Start()
+    {
+
+        _Navmesh = player.GetComponent<NavMeshAgent>();
+        
+        Accessor<TargetEdible>.Instance().AddModule(this);
+
+    }
+
+    public override void Run()
+    {
+        _Navmesh.SetDestination(this.transform.position);
+    }
+}
