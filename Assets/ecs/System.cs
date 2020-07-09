6fe4ed26eaf95e91eb3333ada4d3df0fd492d2cb
AlanBretelle <bretellealan@hotmail.fr>
2020-07-09 13:00:50 +0200

    ecs

---
+++ Assets/ecs/System.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class System : IUpdater
+{
+    public void Run()
+    {
+        Accessor<Composant> modules = new Accessor<Composant>();
+        foreach (var module in modules.GetAllModules())
+        {
+            GameObject go = module;
+            
+        }
+    }
+}
+
+public interface IUpdater
+{
+    void Run();
+}
